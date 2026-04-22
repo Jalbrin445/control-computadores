9700d567d3132cb4fb766d0cfbdd862c5a73e488
Jalbrin445 <mezaguzmanjuanalbrin@gmail.com>
2026-04-22 01:15:31 -0500

    Mejorar la interfaz de usuario y la funcionalidad de inicio de sesión en la aplicación de registro de computadores

--- interfazComputadores.py
+++ interfazComputadores.py
@@ -1,47 +1,106 @@
+"""
+La aplicación debe:
+
+1. Registrar computadores en mantenimiento.
+2. Guardarlos en una lista interna.
+3. Seleccionar un computador para registrar
+4. Calcular y mostrar el costo total.
+5. Validar correctamente las horas.
+
+
+"""
+
+
+
 # Importación de modulos
 import tkinter as tk
-from tkinter import messagebox,ttk
+from tkinter import messagebox,ttk # Estos métodos me sirve para mostrar mensajes de error
+from datetime import datetime
+
 # Definición de clases, variables y más.
+
+ANCHO = 400
+ALTO = 500
+
+class Usuario():
+    def __init__(self, usuario="", password=""):
+        self.__password = password
+        self.__usuario = usuario
+    
+    def validar(self, usuario_ingresado, password_ingresada):
+        self.__usuario = usuario_ingresado
+        if ((usuario_ingresado.lower() == "programacion") and (password_ingresada.lower() == "programacion")):
+            return True
+        else:
+            return False
+    
+    def obtenerUser(self):
+        return self.__usuario
+        
+usuario = Usuario()
 def DashboardPrincipal():
     global ventanaJAMGDashboard
     ventanaJAMGDashboard = tk.Tk()
     ventanaJAMGDashboard.title("Aplicación Registro de Computadores")
+    etiqueta_saludo_bienvenida = tk.Label(ventanaJAMGDashboard, font=("Times New Roman", 20, "bold"),text=f"Bienvenido {usuario.obtenerUser()}")
+    etiqueta_saludo_bienvenida.grid(row=0, column=0, pady= 20)
+    ventanaJAMGDashboard.mainloop()
 
 def PantallaInicial():
 
     global ventanaJAMGInicioSesion, entry_passwordJAMG, entry_usernameJAMG
     ventanaJAMGInicioSesion = tk.Tk()
-    ventanaJAMGInicioSesion.title("Aplicación Registro de Computadores")
-    # ventana de inicio de sesión
 
+    ventanaJAMGInicioSesion.grid_columnconfigure(0, weight=1)
+    ventanaJAMGInicioSesion.grid_rowconfigure(0, weight=1)
+    ventanaJAMGInicioSesion.grid_rowconfigure(6, weight=1)
+
+    ancho_monitor = (ventanaJAMGInicioSesion.winfo_screenwidth())
+    alto_monitor = (ventanaJAMGInicioSesion.winfo_screenheight())
+    x = (ancho_monitor/2) - (ANCHO/2)
+    y = (alto_monitor/2) - (ALTO/2)
+    ventanaJAMGInicioSesion.geometry(f"{ANCHO}x{ALTO}+{int(x)}+{int(y)}")
+    ventanaJAMGInicioSesion.title("Aplicación Registro de Computadores")
+    # Labels y entrys de ventana de inicio de sesión
+    frame_login = tk.Frame(ventanaJAMGInicioSesion)
+    frame_login.grid(row=1, column=0)
     ## Sesión de nombre de usuario
-    label_usernameJAMG = tk.Label(ventanaJAMGInicioSesion, text="Username")
-    label_usernameJAMG.grid(row=0, column=0, padx=100, pady=10)
-    entry_usernameJAMG = tk.Entry(ventanaJAMGInicioSesion,width=30)
-    entry_usernameJAMG.grid(row=1, column=0, padx=100, pady=10)
+    label_usernameJAMG = tk.Label(frame_login, font=("Times New Roman", 14, "bold"),text="Username")
+    label_usernameJAMG.grid(row=1, column=0, padx=10, pady=10)
+    entry_usernameJAMG = tk.Entry(frame_login,width=30)
+    entry_usernameJAMG.grid(row=2, column=0, padx=10, pady=10)
 
     # Sesión de contraseña
 
-    label_passwordJAMG = tk.Label(ventanaJAMGInicioSesion, text="Password")
-    label_passwordJAMG.grid(row=2, column=0, padx=100, pady=10)
-    entry_passwordJAMG = tk.Entry(ventanaJAMGInicioSesion, width=30,show="*")
-    entry_passwordJAMG.grid(row=3, column=0, padx=100, pady=10)
+    label_passwordJAMG = tk.Label(frame_login, font=("Times New Roman", 14, "bold"),text="Password")
+    label_passwordJAMG.grid(row=3, column=0, padx=10, pady=10)
+    entry_passwordJAMG = tk.Entry(frame_login, width=30,show="*")
+    entry_passwordJAMG.grid(row=4, column=0, padx=10, pady=10)
 
-    buttonJAMG = ttk.Button(ventanaJAMGInicioSesion,text="Login", command=Login)
-    buttonJAMG.grid(row=4, column=0, padx=100, pady=10)
+    # Sección del botón para loguearse
+    buttonJAMG = ttk.Button(frame_login,text="Login", command=Login)
+    buttonJAMG.grid(row=5, column=0, padx=100, pady=10)
+    # Sección de información personal
+    etiqueta_informacion_personal = tk.Label(ventanaJAMGInicioSesion, 
+        font=("Times New Roman", 14, "bold"), 
+        text="Juan Albrin Meza Guzmán\nIngeniería Electrónica\nUniversidad Nacional Abierta y A Distancia",
+        justify="center")
+    etiqueta_informacion_personal.grid(row=6, column=0, pady= 20)
     ventanaJAMGInicioSesion.mainloop()
     
 class ComputadorMantenimiento:
     def __init__(self, codigo, hora_entrada, valor_entrada):
-        self._codigo = codigo
-        self._hora_entrada = hora_entrada
-        self._valor_hora = valor_entrada
+        self.__codigo = codigo
+        self.__hora_entrada = hora_entrada
+        self.__valor_hora = valor_entrada
         
 
     def registrar_entrada(self, codigo, valor_entrada,hora):
-        self._codigo = codigo
-        
-    def registrar_salida(hora_salida):
+        self.__codigo = codigo
+        self.__hora_entrada = hora
+        self.__valor_hora = valor_entrada
+
+    def registrar_salida(self, hora_salida):
         pass
     def calcular_valor(self, hora_salida):
         pass
@@ -51,9 +110,11 @@ class ComputadorMantenimiento:
 
 
 def Login():
+    global usernameJAMG
     passwordJAMG = entry_passwordJAMG.get()
     usernameJAMG = entry_usernameJAMG.get()
-    if ((passwordJAMG.lower() == "programacion") and (usernameJAMG.lower() == "programacion")):
+    validacion_inicio_sesionJAMG = usuario.validar(usernameJAMG,passwordJAMG)
+    if (validacion_inicio_sesionJAMG== True):
         ventanaJAMGInicioSesion.destroy()
         DashboardPrincipal()
     else:
